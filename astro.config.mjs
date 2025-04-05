4481ee7ef5dd2716007694d1e5553974f1d5abaa
Joaquin Coronado Ramirez <joaquincora@hotmail.com>
2025-04-05 03:31:42 -0600

    ssr removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-// import node from '@astrojs/node';
-import netlify from '@astrojs/netlify';
-// @ts-check
+// // import node from '@astrojs/node';
+// import netlify from '@astrojs/netlify';
+// // @ts-check
 import { defineConfig } from 'astro/config';
+export default defineConfig({});
 
-// https://astro.build/config
-export default defineConfig({
-  output: 'server',
-  // server: {
-  //   host: true,
-  // },
-  // adapter: node({ mode: 'standalone' }),
-  adapter: netlify(),
-});
+// // https://astro.build/config
+// export default defineConfig({
+//   output: 'server',
+//   // server: {
+//   //   host: true,
+//   // },
+//   // adapter: node({ mode: 'standalone' }),
+//   adapter: netlify(),
+// });
