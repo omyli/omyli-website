f158110df91668f959b6999884b991429a25c418
Joaquin Coronado Ramirez <joaquincora@hotmail.com>
2025-04-10 02:11:49 -0600

    fix script bug

---
+++ src/scripts/splide-init.js
@@ -0,0 +1,30 @@
+import Splide from "@splidejs/splide";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const main = new Splide("#main-slider", {
+    type: 'fade',
+    heightRatio: 0.5,
+    pagination: false,
+    arrows: true,
+    cover: false,
+  });
+
+  const thumbnails = new Splide("#thumbnail-slider", {
+    fixedWidth: 100,
+    height: 64,
+    isNavigation: true,
+    gap: 10,
+    focus: 'number',
+    pagination: false,
+    cover: true,
+    arrows: false,
+    breakpoints: {
+      640: {
+        fixedWidth: 66,
+        height: 48,
+      },
+    },
+  }).mount();
+
+  main.sync(thumbnails).mount();
+});
